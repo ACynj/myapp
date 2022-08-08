153fd37c4e4539d122b3949f5fc4824668473636
Acynj <1246434316@qq.com>
2022-08-08 09:48:20 +0800

    引用submodule

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Button type="primary">按钮</Button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Button from '../submoduleTest/hmui/src/components/button.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Button
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
